0f4167801773dc0c9555456e39096cd3d7a15adf
@lucas.felipe <l17.lucas@gmail.com>
2021-08-26 13:40:39 -0300

    git inicial

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        
+
+        [Required(ErrorMessage ="campo de título obrigatório")]
+        public string titulo { get; set; }
+        [StringLength(30,ErrorMessage ="no máximo 30 caracteres")]
+        public string diretor { get; set; }
+        public string genero { get; set; }
+        [Range(1,600,ErrorMessage ="filme deve ter duração de 1 minuto a 600 minutos")]
+        public int duracao { get; set; }
+        [Key]
+        [Required]
+        public int Id { get;  set; }
+    }
+}
---
+++ FilmesAPI/Properties/FilmeController.cs
@@ -0,0 +1,44 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Properties
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController:ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id_filme=1;
+
+        [HttpPost]
+        public IActionResult adicionaFilme([FromBody]Filme filme) 
+        {
+            filme.Id = id_filme++;
+            filmes.Add(filme);
+            
+            return CreatedAtAction(nameof(RecuperaFilmePorId),new {Id=filme.Id },filme);
+        }
+
+        [HttpGet]
+        public IActionResult recuperaFilme()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id) 
+        {
+            Filme filme=filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme!=null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+
+    }
+}
